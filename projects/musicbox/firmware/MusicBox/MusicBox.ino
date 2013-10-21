81c0809d7a5715d945ffafc57bb2f596cfe8f857
mutac <mutac@github>
2013-10-20 23:26:01 -0700

    .

---
+++ projects/musicbox/firmware/MusicBox/MusicBox.ino
@@ -0,0 +1,29 @@
+
+#include <MIDI.h>
+
+//
+// Serial MIDI device:
+//  Baud: 31250
+//  Channel: 1
+
+static const int sMidiChannel = 1;
+static const int sDotCount = 13;
+static char sPitchMap[sDotCount];
+
+static const int sMidiBegin = 30;
+static const int sMidiEnd = 85;
+
+void setup()
+{
+  for (int dot = 0; dot < sDotCount; dot++)
+  {
+    sPitchMap[dot] = map(dot, 0, sDotCount - 1, sMidiBegin, sMidiEnd);
+  }
+  
+  MIDI.begin(sMidiChannel);
+}
+
+void loop()
+{
+}
+
