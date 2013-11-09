b2af783d5eb4fe505cbe21936cee8d7b8a057b9f
mutac <mutac@github>
2013-11-08 20:20:29 -0800

    Support transition velocity and multiple sensors

--- projects/musicbox/firmware/MusicBox/MusicBox.ino
+++ projects/musicbox/firmware/MusicBox/MusicBox.ino
@@ -24,12 +24,12 @@ public:
   
   void sendNoteOn(byte channel, byte key, byte velocity)
   {
-    sendCommand(0x80, channel, key, velocity);
+    sendCommand(0x90, channel, key, velocity);
   }
 
   void sendNoteOff(byte channel, byte key, byte velocity)
   {
-    sendCommand(0x90, channel, key, velocity);
+    sendCommand(0x80, channel, key, velocity);
   }
   
 private:
@@ -43,6 +43,89 @@ private:
   Stream& mStream;
 };
 
+class MusicBoxPip
+{
+public:
+  static const int kMaxTime = 1000;
+  static const int kMinVelocity = 1;
+  static const int kMaxVelocity = 128;
+  
+  enum Transition
+  {
+    eTransitioned_None,
+    eTransitioned_On,
+    eTransitioned_Off
+  };
+
+public:
+  MusicBoxPip() :
+    mPin(0),
+    mVelocity(0),
+    mWasOn(false),
+    mStartTime(0)
+  {
+  }
+  
+  void begin(int pin)
+  {
+    mPin = pin;
+    pinMode(mPin, INPUT);
+    mWasOn = isOnNow();
+    mStartTime = millis();
+  }
+  
+  Transition checkTransition()
+  {
+    bool isOn = isOnNow();
+    Transition transition = eTransitioned_None;
+    
+    if (!mWasOn && isOn)
+    {
+      calculateTransitionVelocity();
+      transition = eTransitioned_On;
+    }
+    else if(mWasOn && !isOn)
+    {
+      calculateTransitionVelocity();
+      transition = eTransitioned_Off;
+    }
+    
+    mWasOn = isOn;
+    return transition;
+  }
+  
+  int transitionVelocity()
+  {
+    return mVelocity;
+  }
+  
+private:
+
+  bool isOnNow()
+  {
+    return digitalRead(mPin) <= 0;
+  }
+
+  void calculateTransitionVelocity()
+  {
+    unsigned long now = millis();
+    unsigned long elapsed = now - mStartTime;
+    mStartTime = now;
+    
+    if (elapsed > kMaxTime)
+    {
+      elapsed = kMaxTime;
+    }
+    
+    mVelocity = map(kMaxTime - elapsed, 0, kMaxTime, kMinVelocity, kMaxVelocity);
+  }
+
+  int mPin;
+  bool mWasOn;
+  unsigned long mStartTime;
+  int mVelocity;
+};
+
 //
 // Serial MIDI device:
 //  Baud: 9600
@@ -52,21 +135,23 @@ MidiStream sMidiStream;
 
 static const int kMidiBaud = 9600;
 static const int kMidiChannel = 0;
-static const int kDotCount = 13;
-static byte sPitchMap[kDotCount];
 
-static const int kMidiBegin = 30;
-static const int kMidiEnd = 85;
-static const int kVelocity = 64;
+static const int kMidiNoteBegin = 52;
+static const int kMidiNoteEnd = 58;
 
-static int sDot = 0;
+static const int kPipCount = 6;
+static MusicBoxPip sMusicBoxPips[kPipCount];
+static byte sPitchMap[kPipCount];
+static int sPinMap[] = {13, 12, 11, 10, 9, 8};
 
 void setup()
 {
+  
   // Seed pitch map table with something...
-  for (int dot = 0; dot < kDotCount; dot++)
+  for (int pip = 0; pip < kPipCount; pip++)
   {
-    sPitchMap[dot] = map(dot, 0, kDotCount - 1, kMidiBegin, kMidiEnd);
+    sMusicBoxPips[pip].begin(sPinMap[pip]);
+    sPitchMap[pip] = map(pip, 0, kPipCount - 1, kMidiNoteBegin, kMidiNoteEnd);
   }
   
   Serial.begin(kMidiBaud);
@@ -75,11 +160,19 @@ void setup()
 
 void loop()
 {
-  sMidiStream.sendNoteOn(kMidiChannel, sPitchMap[sDot], kVelocity);
-  delay(100);
-  sMidiStream.sendNoteOff(kMidiChannel, sPitchMap[sDot], kVelocity);
-  delay(1000);
-  
-  sDot = (sDot + 1) % kDotCount;
+  for (int pip = 0; pip < kPipCount; pip++)
+  {
+    MusicBoxPip::Transition transition = sMusicBoxPips[pip].checkTransition();
+    
+    if (transition == MusicBoxPip::eTransitioned_On)
+    {
+      sMidiStream.sendNoteOn(kMidiChannel, sPitchMap[pip], sMusicBoxPips[pip].transitionVelocity());
+    }
+    else if (transition == MusicBoxPip::eTransitioned_Off)
+    {
+      sMidiStream.sendNoteOff(kMidiChannel, sPitchMap[pip], sMusicBoxPips[pip].transitionVelocity());
+    }
+  }
 }
+
 
